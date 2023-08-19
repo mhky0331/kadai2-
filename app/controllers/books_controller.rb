912e8617ddac707e71188c3fca610c6890f9dce9
KoyaMiyahara <141590159+mhky0331@users.noreply.github.com>
2023-08-19 11:02:23 +0000

    課題提出用”

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,13 +5,19 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+       flash[:notice] = "successfully create in"
+       redirect_to book_path(@book.id)
+    else
+       @books = Book.all
+       render :index
+    end
   end
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
@@ -23,9 +29,20 @@ class BooksController < ApplicationController
   end
 
   def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+       flash[:notice] = "successfully update in"
+       redirect_to book_path(@book.id)
+    else
+
+       render :edit
+    end
+  end
+
+  def destroy
     book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    book.destroy
+    redirect_to '/books'
   end
 
   private
